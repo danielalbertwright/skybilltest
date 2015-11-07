88499f21737e9189ef093b22ef2d17772e00b7e6
Daniel Wright <Daniel.Wright@atos.net>
2015-11-07 17:58:19 +0000

    Date and cost formatters. Updated Bill models to use formatters in mutators.

---
+++ app/models/FormatUtils.scala
@@ -0,0 +1,18 @@
+package models
+
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+object FormatUtils {
+
+  def formatDateForUser(string: String): String = {
+    val fmt = DateTimeFormat.forPattern("yyyy-MM-dd")
+    val dateTime = DateTime.parse(string, fmt)
+    dateTime.toString("dd/MM/yyyy")
+  }
+
+  def formatCostForUser(cost: Double) : String = {
+    f"$cost%2.2f"
+  }
+
+}
--- app/models/Statement.scala
+++ app/models/Statement.scala
@@ -1,58 +1,80 @@
 package models
 
 import play.api.libs.json.Json
+import models.FormatUtils._
 
 // http://json2caseclass.cleverapps.io/ is very cool!
+
 
 case class Period(
                    from: String,
                    to: String
-                   )
+                   ) {
+  def from_ = formatDateForUser(from)
+  def to_ = formatDateForUser(to)
+}
 
 case class Statement(
                       generated: String,
                       due: String,
                       period: Period
-                      )
+                      ) {
+  def generated_ = formatDateForUser(generated)
+  def due_ = formatDateForUser(due)
+}
 
 case class Subscription(
 
-                          `type`: String,
-                          name: String,
-                          cost: Double
-                          )
+                         `type`: String,
+                         name: String,
+                         cost: Double
+                         ) {
+  def cost_ = formatCostForUser(cost)
+}
 
 case class Package(
                     subscriptions: List[Subscription],
                     total: Double
-                    )
+                    ) {
+  def total_ = formatCostForUser(total)
+}
 
 case class Call(
-                  called: String,
-                  duration: String,
-                  cost: Double
-                  )
+                 called: String,
+                 duration: String,
+                 cost: Double
+                 ) {
+  def cost_ = formatCostForUser(cost)
+}
 
 case class CallCharges(
                         calls: List[Call],
                         total: Double
-                        )
+                        ) {
+  def total_ = formatCostForUser(total)
+}
 
 case class Rental(
-                    title: String,
-                    cost: Double
-                    )
+                   title: String,
+                   cost: Double
+                   ) {
+  def cost_ = formatCostForUser(cost)
+}
 
 case class BuyAndKeep(
-                    title: String,
-                    cost: Double
-                    )
+                       title: String,
+                       cost: Double
+                       ) {
+  def cost_ = formatCostForUser(cost)
+}
 
 case class SkyStore(
                      rentals: List[Rental],
                      buyAndKeep: List[BuyAndKeep],
                      total: Double
-                     )
+                     ) {
+  def total_ = formatCostForUser(total)
+}
 
 case class Bill(
                  statement: Statement,
@@ -60,15 +82,46 @@ case class Bill(
                  `package`: Package,
                  callCharges: CallCharges,
                  skyStore: SkyStore
-                 )
+                 ) {
+  def total_ = formatCostForUser(total)
+}
 
-object BuyAndKeep {implicit val formatter = Json.format[BuyAndKeep]}
-object Rental {implicit val formatter = Json.format[Rental]}
-object Call {implicit val formatter = Json.format[Call]}
-object Subscription {implicit val formatter = Json.format[Subscription]}
-object Period {implicit val formatter = Json.format[Period]}
-object SkyStore {implicit val formatter = Json.format[SkyStore]}
-object CallCharges {implicit val formatter = Json.format[CallCharges]}
-object Package {implicit val formatter = Json.format[Package]}
-object Statement {implicit val formatter = Json.format[Statement]}
-object Bill {implicit val formatter = Json.format[Bill]}
+object BuyAndKeep {
+  implicit val formatter = Json.format[BuyAndKeep]
+}
+
+object Rental {
+  implicit val formatter = Json.format[Rental]
+}
+
+object Call {
+  implicit val formatter = Json.format[Call]
+}
+
+object Subscription {
+  implicit val formatter = Json.format[Subscription]
+}
+
+object Period {
+  implicit val formatter = Json.format[Period]
+}
+
+object SkyStore {
+  implicit val formatter = Json.format[SkyStore]
+}
+
+object CallCharges {
+  implicit val formatter = Json.format[CallCharges]
+}
+
+object Package {
+  implicit val formatter = Json.format[Package]
+}
+
+object Statement {
+  implicit val formatter = Json.format[Statement]
+}
+
+object Bill {
+  implicit val formatter = Json.format[Bill]
+}
