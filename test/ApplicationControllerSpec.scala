9cc5120b77306bbee8a0abf30dbf64130a6da71b
Daniel Wright <Daniel.Wright@atos.net>
2015-11-08 11:22:01 +0000

    Application controller is going to serve bill2.

--- test/ApplicationControllerSpec.scala
+++ test/ApplicationControllerSpec.scala
@@ -12,5 +12,15 @@ class ApplicationControllerSpec extends PlaySpecification with Results {
       val result = controller.index().apply(FakeRequest())
       contentAsString(result) must contain("PageId:index")
     }
+    "serve the bill1 page" in {
+      val controller = new controllers.Application
+      val result = controller.bill1().apply(FakeRequest())
+      contentAsString(result) must contain("PageId:bill1")
+    }
+    "serve the bill2 page" in {
+      val controller = new controllers.Application
+      val result = controller.bill2().apply(FakeRequest())
+      contentAsString(result) must contain("PageId:bill2")
+    }
   }
 }
