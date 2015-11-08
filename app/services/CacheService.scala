3d0ad40dca4fafd9323ccba3b90a896b3d3b8802
Daniel Wright <Daniel.Wright@atos.net>
2015-11-08 15:00:18 +0000

    Refactored cache to store the bill as json instead of as a bill. This is so that it can be used in page 3 where an ajax call is made to the server to get the bill instead of doing server side rendering.

--- app/services/CacheService.scala
+++ app/services/CacheService.scala
@@ -4,28 +4,32 @@ import models.Bill
 import play.api.Logger
 import play.api.Play.current
 import play.api.cache.Cache
-import scala.concurrent.duration._
+import play.api.libs.json.JsValue
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 class CacheService {
 
   val api = new Api
 
-  def getBill: Future[Bill] = {
-    Logger.debug("Getting bill from cache")
-    Cache.getAs[Bill]("bill") match {
-      case Some(bill: Bill) =>
+  def getJson: Future[JsValue] = {
+    Logger.debug("Getting json from cache")
+    Cache.getAs[JsValue]("bill") match {
+      case Some(bill: JsValue) =>
         Logger.debug("Cache hit")
         Future.successful(bill)
       case None =>
         Logger.debug("Cache miss")
         api.get("http://safe-plains-5453.herokuapp.com/bill.json").map {
           wsresponse =>
-            Cache.set("bill", wsresponse.json.as[Bill], 5 minutes)
-            wsresponse.json.as[Bill]
+            Cache.set("bill", wsresponse.json, 5 minutes)
+            wsresponse.json
         }
     }
   }
+
+  def getBill: Future[Bill] = getJson.map(json => json.as[Bill])
 
 }
--- test/CacheServiceSpec.scala
+++ test/CacheServiceSpec.scala
@@ -1,39 +1,37 @@
 import models.Bill
 import org.specs2.mock.Mockito
 import play.api.cache.Cache
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.JsValue
 import play.api.libs.ws.{WSCookie, WSResponse}
 import play.api.test.{PlaySpecification, WithApplication}
 import services.{Api, CacheService}
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.xml.Elem
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class CacheServiceSpec extends PlaySpecification with Mockito {
 
-  val testJson = TestModels.sampleJson
-
   "CacheService" should {
-    "When the cache contains the bill, it should return a bill." in new WithApplication {
+    "When the cache contains the bill json, it should return a bill in json format." in new WithApplication {
 
-      val bill = testJson.as[Bill]
-
-      Cache.set("bill", bill)
+      Cache.set("bill", TestModels.sampleJson)
 
       val mockApi = mock[Api]
+
       class TestCacheService extends CacheService {
         override val api = mockApi
       }
 
       val billCacheService = new TestCacheService
 
-      val billResponse: Bill = Await.result(billCacheService.getBill, 1 second)
-      billResponse must_== testJson.as[Bill]
+      val billResponse: JsValue = Await.result(billCacheService.getJson, 1 second)
+      billResponse must_== TestModels.sampleJson
       there was no(mockApi).get(anyString)
     }
 
-    "When the cache doesn't contain a bill, it should get the bill from the API, update the cache and return a bill." in new WithApplication {
+    "When the cache doesn't contain a bill, it should get the bill from the API, update the cache and return a bill in json format." in new WithApplication {
       Cache.remove("bill")
       val response = new WSResponse {
         override def statusText: String = ???
@@ -54,7 +52,64 @@ class CacheServiceSpec extends PlaySpecification with Mockito {
 
         override def status: Int = ???
 
-        override def json: JsValue = testJson
+        override def json: JsValue = TestModels.sampleJson
+
+        override def allHeaders: Map[String, Seq[String]] = ???
+      }
+
+      val mockApi = mock[Api]
+      mockApi.get(anyString) returns Future(response)
+
+      class TestCacheService extends CacheService {
+        override val api = mockApi
+      }
+
+      val testCacheService = new TestCacheService
+
+      val billResponse: JsValue = Await.result(testCacheService.getJson, 1 second)
+      billResponse must_== TestModels.sampleJson
+      there was one(mockApi).get(anyString)
+    }
+
+    "When the cache contains the bill json, it should return a bill." in new WithApplication {
+
+      Cache.set("bill", TestModels.sampleJson)
+
+      val mockApi = mock[Api]
+
+      class TestCacheService extends CacheService {
+        override val api = mockApi
+      }
+
+      val billCacheService = new TestCacheService
+
+      val billResponse: Bill = Await.result(billCacheService.getBill, 1 second)
+      billResponse must_== TestModels.sampleBill
+      there was no(mockApi).get(anyString)
+    }
+
+    "When the cache doesn't contain a bill, it should get the bill from the API, update the cache and return a bill in json format." in new WithApplication {
+      Cache.remove("bill")
+      val response = new WSResponse {
+        override def statusText: String = ???
+
+        override def underlying[T]: T = ???
+
+        override def xml: Elem = ???
+
+        override def body: String = ???
+
+        override def header(key: String): Option[String] = ???
+
+        override def cookie(name: String): Option[WSCookie] = ???
+
+        override def bodyAsBytes: Array[Byte] = ???
+
+        override def cookies: Seq[WSCookie] = ???
+
+        override def status: Int = ???
+
+        override def json: JsValue = TestModels.sampleJson
 
         override def allHeaders: Map[String, Seq[String]] = ???
       }
@@ -69,8 +124,9 @@ class CacheServiceSpec extends PlaySpecification with Mockito {
       val testCacheService = new TestCacheService
 
       val billResponse: Bill = Await.result(testCacheService.getBill, 1 second)
-      billResponse must_== testJson.as[Bill]
+      billResponse must_== TestModels.sampleBill
       there was one(mockApi).get(anyString)
     }
+
   }
 }
