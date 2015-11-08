7202781ab528dd01466f5ae96afadf53c3d775f1
Daniel Wright <Daniel.Wright@atos.net>
2015-11-08 12:55:38 +0000

    Implemented show/hide functionality on bill2, added the ability to pass scripts in to the main template and cleaned up page design.

--- public/javascripts/bill2.js
+++ public/javascripts/bill2.js
@@ -1,3 +1,24 @@
-function helloWorld() {
-  return "Hello world!";
-}
+function slideSection(link, section) {
+    $(section).slideToggle();
+    var state = $(link).text();
+    if(state=="Show") {
+      $(link).text("Hide");
+    } else {
+      $(link).text("Show");
+    }
+  }
+
+$(function() {
+  // When a user clicks the packages toggle.
+  $("#package-toggle").click(function() {
+    slideSection("#package-toggle",".package");
+  });
+  $("#call-charges-toggle").click(function() {
+    slideSection("#call-charges-toggle",".call-charges");
+  });
+  $("#sky-store-toggle").click(function() {
+    slideSection("#sky-store-toggle",".sky-store");
+  });
+});
+
+
